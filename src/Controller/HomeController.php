f2aa5c63a73a94730d8ef4c38dd5a693b1acf819
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2019-12-03 15:11:35 +0100

    Added home route

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright (C) 2019 Gigadrive - All rights reserved.
+ * https://gigadrivegroup.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://gnu.org/licenses/>
+ */
+
+namespace TwitchArchive\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController {
+	/**
+	 * @Route("/")
+	 */
+	public function indexAction() {
+		return $this->render("home.html.twig");
+	}
+}
